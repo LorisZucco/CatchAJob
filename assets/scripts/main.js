1fac80b9c6a5f0166b27b0d67d987f64b38520bf
LorisZucco <lorisjunior@gmail.com>
2026-09-25 22:49:15 -0300

    feat: implementa formulário para candidatos e ajusta estrutura HTML

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+import { mostrarFormularioCandidato } from "./ui.js";
+
+const btnCandidato = document.getElementById("btn-candidato");
+
+btnCandidato.addEventListener("click", mostrarFormularioCandidato);
